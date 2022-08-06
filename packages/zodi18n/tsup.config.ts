d2b01e160fcf785f8e690a125eba58508cb391fb
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-06 13:24:41 +0900

    add tsup and build/dev scripts on core package

---
+++ packages/zodi18n/tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default defineConfig([
+  {
+    entry: ["src/index.ts", "src/languages/*.ts"],
+    clean: true,
+    dts: true,
+    format: ["cjs", "esm"],
+    minify: isProduction,
+    sourcemap: true,
+  },
+]);
