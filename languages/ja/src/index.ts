c24bec3d4c18997ecfc16e322bcf541f1d410589
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-06 13:01:14 +0900

    add build script on zodi18n-ja

---
+++ languages/ja/src/index.ts
@@ -0,0 +1,9 @@
+export const translation = {
+  zod: {
+    invalid_type_required: "必須",
+    invalid_type:
+      "{{expected}}での入力を期待していますが、{{received}}で入力されました。",
+
+    // TODO: expected and received type
+  },
+};
