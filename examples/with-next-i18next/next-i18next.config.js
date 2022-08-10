bb7e9a732c4fb05c5ddb46ef301a24772982fd25
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-11 02:53:34 +0900

    chore: update example `with-next-i18next`

--- examples/with-next-i18next/next-i18next.config.js
+++ examples/with-next-i18next/next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja"],
+    localePath: path.resolve("./public/locales"),
     interpolation: {
       skipOnVariables: false,
     },
