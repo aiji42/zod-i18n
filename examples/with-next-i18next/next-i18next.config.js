1256792e5f5f17b21d15b05e51782b9b398b9b46
Luiz Oliveira Montedonio <montedonio.luiz@gmail.com>
2022-11-22 12:20:21 -0300

    feat: example files for portuguese

--- examples/with-next-i18next/next-i18next.config.js
+++ examples/with-next-i18next/next-i18next.config.js
@@ -6,7 +6,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "ja", "fr", "ar"],
+    locales: ["en", "ja", "fr", "ar", "pt"],
     localePath: path.resolve("./public/locales"),
     interpolation: {
       skipOnVariables: false,
