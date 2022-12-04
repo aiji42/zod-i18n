676543d0ba164f9da0441c9f4999cf1b6ce1f380
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-12-04 23:29:06 +0900

    chore: bumpup

--- examples/with-next-i18next/next-i18next.config.js
+++ examples/with-next-i18next/next-i18next.config.js
@@ -1,5 +1,3 @@
-const path = require("path");
-
 /**
  * @type {import('next-i18next').UserConfig}
  */
@@ -7,10 +5,6 @@ module.exports = {
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja", "fr", "ar", "pt", "zh-CN"],
-    localePath: path.resolve("./public/locales"),
-    interpolation: {
-      skipOnVariables: false,
-    },
   },
   reloadOnPrerender: true,
 };
