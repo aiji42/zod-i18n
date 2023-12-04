c800e5f424ecc9cf216f06afeee0826a3ce32562
yijie4188 <yijie4188@gmail.com>
2023-12-04 16:52:22 +0800

    feat(zh-CN): update unit test and translate

--- packages/core/tests/integrations/zh-CN.test.ts
+++ packages/core/tests/integrations/zh-CN.test.ts
@@ -11,86 +11,88 @@ beforeAll(async () => {
 test("string parser error messages", () => {
   const schema = z.string();
 
-  expect(getErrorMessage(schema.safeParse(undefined))).toEqual("必填项");
+  expect(getErrorMessage(schema.safeParse(undefined))).toEqual("必填");
   expect(getErrorMessage(schema.safeParse(1))).toEqual(
-    "期望输入的是字符串, 而输入的是数字"
+    "预期输入为字符串，而输入为数字"
   );
   expect(getErrorMessage(schema.safeParse(true))).toEqual(
-    "期望输入的是字符串, 而输入的是布尔值"
+    "预期输入为字符串，而输入为布尔值"
   );
   expect(getErrorMessage(schema.safeParse(Date))).toEqual(
-    "期望输入的是字符串, 而输入的是函数"
+    "预期输入为字符串，而输入为函数"
   );
   expect(getErrorMessage(schema.safeParse(new Date()))).toEqual(
-    "期望输入的是字符串, 而输入的是日期"
+    "预期输入为字符串，而输入为日期"
   );
-  expect(getErrorMessage(schema.email().safeParse(""))).toEqual("邮件格式错误");
-  expect(getErrorMessage(schema.url().safeParse(""))).toEqual("url 格式错误");
+  expect(getErrorMessage(schema.email().safeParse(""))).toEqual(
+    "错误的邮件格式"
+  );
+  expect(getErrorMessage(schema.url().safeParse(""))).toEqual("错误的链接格式");
   expect(getErrorMessage(schema.regex(/aaa/).safeParse(""))).toEqual(
-    "格式错误"
+    "错误的格式"
   );
   expect(getErrorMessage(schema.startsWith("foo").safeParse(""))).toEqual(
-    '必须以 "foo" 起始'
+    '文本必须以 "foo" 开头'
   );
   expect(getErrorMessage(schema.endsWith("bar").safeParse(""))).toEqual(
-    '必须以 "bar" 结尾'
+    '文本必须以 "bar" 结尾'
   );
   expect(getErrorMessage(schema.min(5).safeParse("a"))).toEqual(
-    "至少需要包含 5 个字符"
+    "文本长度不得少于 5 个字符"
   );
   expect(getErrorMessage(schema.max(5).safeParse("abcdef"))).toEqual(
-    "最多只能包含 5 个字符"
+    "文本长度不得多于 5 个字符"
   );
-  // TODO: add `zod:errors.(too_small|too_big).string.exact`
   expect(getErrorMessage(schema.length(5).safeParse("abcdef"))).toEqual(
-    "String must contain exactly 5 character(s)"
+    "文本长度必须为 5 个字符"
   );
-  // TODO: translation `datetime` (zod:validations.datetime)
   expect(
     getErrorMessage(schema.datetime().safeParse("2020-01-01T00:00:00+02:00"))
-  ).toEqual("datetime 格式错误");
+  ).toEqual("错误的日期时间格式");
 });
 
 test("number parser error messages", () => {
   const schema = z.number();
 
-  expect(getErrorMessage(schema.safeParse(undefined))).toEqual("必填项");
+  expect(getErrorMessage(schema.safeParse(undefined))).toEqual("必填");
   expect(getErrorMessage(schema.safeParse(""))).toEqual(
-    "期望输入的是数字, 而输入的是字符串"
+    "预期输入为数字，而输入为字符串"
   );
   expect(getErrorMessage(schema.safeParse(null))).toEqual(
-    "期望输入的是数字, 而输入的是null"
+    "预期输入为数字，而输入为空对象"
   );
   expect(getErrorMessage(schema.safeParse(NaN))).toEqual(
-    "期望输入的是数字, 而输入的是NaN"
+    "预期输入为数字，而输入为非数"
   );
   expect(getErrorMessage(schema.int().safeParse(0.1))).toEqual(
-    "期望输入的是整数, 而输入的是浮点数"
+    "预期输入为整数，而输入为浮点数"
   );
   expect(getErrorMessage(schema.multipleOf(5).safeParse(2))).toEqual(
-    "必须是 5 的倍数"
+    "数值必须是 5 的倍数"
   );
   expect(getErrorMessage(schema.step(0.1).safeParse(0.0001))).toEqual(
-    "必须是 0.1 的倍数"
+    "数值必须是 0.1 的倍数"
   );
-  expect(getErrorMessage(schema.lt(5).safeParse(10))).toEqual("必须小于 5");
+  expect(getErrorMessage(schema.lt(5).safeParse(10))).toEqual("数值必须小于 5");
   expect(getErrorMessage(schema.lte(5).safeParse(10))).toEqual(
-    "必须小于或等于 5"
+    "数值不得大于 5"
   );
-  expect(getErrorMessage(schema.gt(5).safeParse(1))).toEqual("必须大于 5");
-  expect(getErrorMessage(schema.gte(5).safeParse(1))).toEqual(
-    "必须大于或等于 5"
-  );
+  expect(getErrorMessage(schema.gt(5).safeParse(1))).toEqual("数值必须大于 5");
+  expect(getErrorMessage(schema.gte(5).safeParse(1))).toEqual("数值不得小于 5");
   expect(getErrorMessage(schema.nonnegative().safeParse(-1))).toEqual(
-    "必须大于或等于 0"
+    "数值不得小于 0"
   );
   expect(getErrorMessage(schema.nonpositive().safeParse(1))).toEqual(
-    "必须小于或等于 0"
+    "数值不得大于 0"
   );
-  expect(getErrorMessage(schema.negative().safeParse(1))).toEqual("必须小于 0");
-  expect(getErrorMessage(schema.positive().safeParse(0))).toEqual("必须大于 0");
+  expect(getErrorMessage(schema.negative().safeParse(1))).toEqual(
+    "数值必须小于 0"
+  );
+  expect(getErrorMessage(schema.positive().safeParse(0))).toEqual(
+    "数值必须大于 0"
+  );
   expect(getErrorMessage(schema.finite().safeParse(Infinity))).toEqual(
-    "不能为无限值"
+    "数值必须有限"
   );
 });
 
@@ -99,7 +101,7 @@ test("date parser error messages", async () => {
   const schema = z.date();
 
   expect(getErrorMessage(schema.safeParse("2022-12-01"))).toEqual(
-    "期望输入的是日期, 而输入的是字符串"
+    "预期输入为日期，而输入为字符串"
   );
   expect(
     getErrorMessage(
@@ -122,7 +124,7 @@ test("array parser error messages", () => {
   const schema = z.string().array();
 
   expect(getErrorMessage(schema.safeParse(""))).toEqual(
-    "期望输入的是数组, 而输入的是字符串"
+    "预期输入为数组，而输入为字符串"
   );
   expect(getErrorMessage(schema.min(5).safeParse([""]))).toEqual(
     "至少需要包含 5 个元素"
