4d34a41ef05c94b2bdd81a3d520814a422938f12
mohammad foroutan <mmdfrn81@gmail.com>
2023-12-23 23:03:42 +0330

    feat: add example for fa locale

--- examples/with-next-i18next/pages/index.tsx
+++ examples/with-next-i18next/pages/index.tsx
@@ -86,6 +86,7 @@ export default function HookForm() {
             <option value="it">italiano</option>
             <option value="fi">suomi</option>
             <option value="fr">Français</option>
+            <option value="fa">فارسی</option>
             <option value="hr-HR">Hrvatski</option>
             <option value="is">íslenskur</option>
             <option value="ko">한국어</option>
